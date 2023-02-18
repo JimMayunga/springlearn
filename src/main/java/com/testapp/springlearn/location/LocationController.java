ef9c75ed60906a8b9dffd159dfb000830f226b76
Jim Mayunga <jimmayunga@gmail.com>
2023-02-18 12:14:28 +0300

    --- user list added to user class

--- src/main/java/com/testapp/springlearn/location/LocationController.java
+++ src/main/java/com/testapp/springlearn/location/LocationController.java
@@ -1,8 +1,11 @@
 package com.testapp.springlearn.location;
 
+
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class LocationController {
+
     
 }
--- src/main/java/com/testapp/springlearn/user/UsersController.java
+++ src/main/java/com/testapp/springlearn/user/UsersController.java
@@ -1,8 +1,33 @@
 package com.testapp.springlearn.user;
 
+import java.util.Arrays;
+import java.util.List;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.testapp.springlearn.location.Location;
 
 @RestController
 public class UsersController {
-    
+
+    @RequestMapping("/users")
+    public List<Users> getAllUsers() {
+
+        Users user1 = new Users(
+                "u1",
+                "Jane",
+                "Doe",
+                new Location("l1", "Nakuru"),
+                "janedoe@gmail.com");
+
+        Users user2 = new Users(
+                "u2",
+                "Jim",
+                "Mayunga",
+                new Location("l2", "Nairobi"),
+                "jimmayunga@gmail.com");
+
+        return Arrays.asList(user1, user2);
+    }
+
 }
